aef1aead137247716c672e58be34995923419c4e
Leonardo Lopes <blopesleo@outlook.com>
2021-01-31 02:04:20 -0300

    Adding Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Monkey D. Luffy', age: 19)
+User.create(name: 'Roronoa Zoro', age: 21)
+User.create(name: 'Nami', age: 20)
+User.create(name: 'Usopp', age: 19)
+User.create(name: 'Sanji', age: 21)
+User.create(name: 'Tony Tony Chopper', age: 17)
+User.create(name: 'Nico Robin', age: 30)
+User.create(name: 'Franky', age: 36)
+User.create(name: 'Brook', age: 90)
+User.create(name: 'Jinbe', age: 46)
